6298e1ad3556034d77533ea98e9719f8951b2c20
Baguette <73280794+SoftBaguette@users.noreply.github.com>
2023-04-20 16:47:48 +0100

    changed some tile values

--- assets/KitchenV2.tsx
+++ assets/KitchenV2.tsx
@@ -3,37 +3,37 @@
  <image source="TileSetV2.png" width="48" height="128"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="worktop" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="worktop" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="1">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="chopping_board" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="chopping_board" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="2">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="pan1" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="pan1" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="3">
   <objectgroup draworder="index" id="5">
-   <object id="4" name="tomato" x="0" y="0" width="16" height="16"/>
+   <object id="4" name="tomato" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="4">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="onion" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="onion" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="5">
   <objectgroup draworder="index" id="3">
-   <object id="2" name="lettuce" x="0" y="0" width="16" height="16"/>
+   <object id="2" name="lettuce" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="6">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="steak" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="steak" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="7">
@@ -43,32 +43,32 @@
  </tile>
  <tile id="8">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="pan2" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="pan2" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="11">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="oven" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="oven" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="12">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="plate" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="plate" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="13">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="bin" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="bin" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="14">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="completed_dish" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="completed_dish" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="15">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="order_bottom_left" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="order_bottom_left" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="16">
@@ -98,17 +98,17 @@
  </tile>
  <tile id="21">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="potato" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="potato" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="22">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="pizza_dough" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="pizza_dough" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
  <tile id="23">
   <objectgroup draworder="index" id="2">
-   <object id="1" name="cheese" x="0" y="0" width="16" height="16"/>
+   <object id="1" name="cheese" x="1" y="1" width="14" height="14"/>
   </objectgroup>
  </tile>
 </tileset>
